c94e367479828b6d6585c7c75fd659574a3b17be
keerthanajanani <keerthijanani66@gmail.com>
2017-07-15 11:31:19 +0530

    Create Numeric char.java

---
+++ Numeric char.java
@@ -0,0 +1,20 @@
+# Numeric-char
+import java.io.*;
+import java.util.*;
+public class Numeric char
+{
+int n,count=0;
+public static void main(String args[])throws IOException
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(br.readLine());
+System.out.println("Enter the Character:");
+String s=br.readLine();
+n=s.length();
+for(int i=0;i<n;i++)
+if(char.isDigit(s.charAt(i))
+{
+count++;
+}
+System.out.println("count the numeric characters is : "+count);
+}
+}
